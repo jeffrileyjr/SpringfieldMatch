4ce2ee758e386897004c6edb7bfd2638b38452d3
jeffrileyjr <49040622+jeffrileyjr@users.noreply.github.com>
2020-12-14 19:54:56 -0500

    fixed bug on matches, fixed sound toggle, cleaned up code

--- index.js
+++ index.js
@@ -1,48 +1,33 @@
-let card = document.getElementsByClassName('card');
+let card = document.getElementsByClassName("card");
 let cards = [...card];
 let openedCards = [];
+let gameStarted = false;
 let matches = [];
 let moves = 0;
 let second = 0,
     minute = 0;
-let timer = document.querySelector('.timer');
+let timer = document.querySelector(".timer");
 let interval = null;
+// audio stuff
+let sound = true;
 let doh = new Audio("./assets/doh.mp3");
 let woowho = new Audio("./assets/woowhoo.mp3");
 let audio = new Audio("./assets/theme.mp3");
+let audioStatus = document.querySelector(".sound");
 // close icon in modal
 let closeicon = document.getElementById("close");
 
 // declare modal
 let modal = document.getElementById("modal");
-
-let beerRating = document.querySelector('.beerRating').children;
-
-let audioStatus = document.querySelector('.sound');
-
-
-
-// let mobileCoverImage = document.getElementsByClassName('cover');
-let dtopCoverImage = document.getElementsByClassName('desktopCover');
-
-let cardImages = document.getElementsByClassName('card-image');
+// rating
+let beerRating = document.querySelector(".beerRating").children;
+// coverImage
+let dtopCoverImage = document.getElementsByClassName("desktopCover");
+// array of images to shuffle
+let cardImages = document.getElementsByClassName("card-image");
 let cardImgArray = [...cardImages];
 
-
-// cards.forEach(el => {
-//     el.addEventListener('click', (e) => {
-//         console.log(e)
-//         if (e.target.nodeName === "DIV") {
-//             e.target.children[0].classList.toggle('showCard');
-//             e.target.classList.toggle('hideCard')
-//         } else {
-//             e.target.parentElement.classList.toggle('hideCard');
-//             e.target.classList.toggle('showCard');
-//         }
-
-//     })
-// })
-
+// shuffle function to randomize cards
 function shuffle(array) {
     let currentIndex = array.length,
         temporaryValue,
@@ -58,17 +43,15 @@ function shuffle(array) {
 
     return array;
 }
-
-
-
+// dispaly card function
 function displayCard() {
-    this.children[0].classList.toggle('showCard');
-    this.children[0].classList.toggle('open');
-    this.children[0].classList.toggle('show');
-    this.classList.toggle('disabled');
     cardOpen(this);
+    this.children[0].classList.toggle("showCard");
+    this.children[0].classList.toggle("open");
+    this.children[0].classList.toggle("show");
+    this.classList.toggle("disabled");
 }
-
+// open cards than check for match if 2 cards are open
 function cardOpen(card) {
     openedCards.push(card);
     let len = openedCards.length;
@@ -83,15 +66,16 @@ function cardOpen(card) {
         }
     }
 }
-
+// move counter
 function moveCounter() {
     moves++;
     if (moves == 1) {
         startTimer();
     }
 }
-
+// match check
 function matched() {
+    disable();
     // add timeout so there is a delay before matches are removed
     setTimeout(function () {
         openedCards[0].classList.add("match");
@@ -100,16 +84,44 @@ function matched() {
         openedCards[1].classList.remove("show", "open");
         matches.push(openedCards[0]);
         matches.push(openedCards[1]);
+        enable();
         openedCards = [];
         if (matches.length == 20) {
             gameOver();
         }
-    }, 1000)
-
+    }, 400);
 }
-
+// reset picks when not matched
+function resetPicks() {
+    openedCards[0].classList.add("unmatched");
+    openedCards[1].classList.add("unmatched");
+    disable();
+    setTimeout(function () {
+        openedCards[0].classList.remove("show", "open", "unmatched");
+        openedCards[1].classList.remove("show", "open", "unmatched");
+        openedCards[0].children[0].classList.remove("showCard");
+        openedCards[1].children[0].classList.remove("showCard");
+        enable();
+        openedCards = [];
+    }, 425);
+}
+// disables click so you can't open more than 2 cards
+function disable() {
+    cards.filter((card, i, cards) => {
+        card.classList.add("disabled");
+    });
+}
+// re-enables cards to be clicked
+function enable() {
+    cards.filter((card) => {
+        card.classList.remove('disabled');
+        for (let i = 0; i < matches.length; i++) {
+            matches[i].classList.add('disabled');
+        }
+    })
+}
+// timer function
 function startTimer() {
-
     interval = setInterval(function () {
         if (second < 10) {
             timer.innerText = "0" + minute + "m   0" + second + "s";
@@ -117,33 +129,32 @@ function startTimer() {
             timer.innerText = "0" + minute + "m  " + second + "s";
         }
 
-        second++
+        second++;
         if (second == 60) {
             minute++;
             second = 0;
         }
     }, 1000);
-
 }
-
+// check for game over and provide rating based on number of moves
 function gameOver() {
     if (matches.length == 20) {
         clearInterval(interval);
         let finalTime = timer.innerHTML;
-        console.log(modal)
-        modal.classList.add("showModal")
+        console.log(modal);
+        modal.classList.add("showModal");
         audio.pause();
         audio.currentTime = 0;
-        document.getElementById('numMoves').innerHTML = moves;
-        document.getElementById('totalTime').innerHTML = finalTime;
+        document.getElementById("numMoves").innerHTML = moves;
+        document.getElementById("totalTime").innerHTML = finalTime;
         closeModal();
 
-        if (moves >= 10 && moves < 14) {
-            beerRating[0].classList.toggle('hideBeers');
-        } else if (moves >= 14 && moves < 19) {
-            beerRating[1].classList.toggle('hideBeers');
-        } else if (moves >= 19) {
-            beerRating[2].classList.toggle('hideBeers');
+        if (moves >= 10 && moves < 15) {
+            beerRating[0].classList.toggle("hideBeers");
+        } else if (moves >= 15 && moves < 20) {
+            beerRating[1].classList.toggle("hideBeers");
+        } else if (moves >= 20) {
+            beerRating[2].classList.toggle("hideBeers");
         }
     }
 }
@@ -156,49 +167,26 @@ function closeModal() {
 }
 
 function resetTimer() {
-    timer.innerHTML = ''
+    timer.innerHTML = "";
     clearInterval(interval);
     minute = 0;
     second = 0;
     timerOn = 0;
 }
 
-function resetPicks() {
-    openedCards[0].classList.add("unmatched");
-    openedCards[1].classList.add("unmatched");
-    disable();
-    setTimeout(function () {
-        openedCards[0].classList.remove("show", "open", "unmatched");
-        openedCards[1].classList.remove("show", "open", "unmatched");
-        openedCards[0].children[0].classList.remove('showCard');
-        openedCards[1].children[0].classList.remove('showCard');
-        enable();
-        openedCards = [];
 
-    }, 1100)
-}
-
-function disable() {
-    cards.filter((card, i, cards) => {
-        card.classList.add('disabled');
-    })
-}
-
-function enable() {
-    cards.filter((card) => {
-        card.classList.remove('disabled');
-        for (let i = 0; i < matches.length; i++) {
-            matches[i].classList.add('disabled');
-        }
-    })
-}
 
 function startGame() {
     // shuffle cards using Fisher-Yates shuffle function
     let shuffledDeck = shuffle(cardImgArray);
+    // sets gameStarted variable to true for sound toggle function
+    gameStarted = true;
+    // checks if sound is on before starting theme
+    if (sound) {
+        audio.play();
+    }
 
-    audio.play();
-    console.log(beerRating)
+    console.log(beerRating);
 
     for (i = 0; i < shuffledDeck.length; i++) {
         //remove all images from previous games from each card (if any)
@@ -207,41 +195,48 @@ function startGame() {
         cards[i].appendChild(shuffledDeck[i]);
         cards[i].type = `${shuffledDeck[i].alt}`;
         // remove extra classes before game starts
-        cards[i].classList.remove('show', 'open', 'match', 'disabled');
-        cards[i].children[0].classList.remove('showCard');
-
+        cards[i].classList.remove("show", "open", "match", "disabled");
+        cards[i].children[0].classList.remove("showCard");
     }
     // add event listener onto the cards
     for (let i = 0; i < cards.length; i++) {
         cards[i].addEventListener("click", displayCard);
     }
     // mobileCoverImage[0].classList.add('hideCover');
-    dtopCoverImage[0].classList.add('hideCover');
+    dtopCoverImage[0].classList.add("hideCover");
 
     // show the cards briefly before game starts
     setTimeout(function () {
         flashCards();
     }, 500);
 }
-
+// toggle sounds on and off and sets text dynamically
 function toggleSound() {
-    if (audio.paused) {
-        audio.play();
-        doh.muted = false;
-        woowho.muted = false;
-        audioStatus.innerHTML = `Sound Off`;
+    if (gameStarted) {
+        if (audio.paused) {
+            audio.play();
+            doh.muted = false;
+            woowho.muted = false;
+            sound = false;
+            audioStatus.innerHTML = `Sound Off`;
+        } else {
+            audio.pause();
+            doh.muted = true;
+            woowho.muted = true;
+            sound = true;
+            audioStatus.innerHTML = `Sound On`;
+        }
     } else {
-        audio.pause();
+        sound = false;
         doh.muted = true;
         woowho.muted = true;
         audioStatus.innerHTML = `Sound On`;
     }
+
 }
 
 function resetGame() {
-    console.log('bacon')
-    // mobileCoverImage[0].classList.remove('hideCover');
-    dtopCoverImage[0].classList.remove('hideCover');
+    dtopCoverImage[0].classList.remove("hideCover");
     resetTimer();
     audio.pause();
     audio.currentTime = 0;
@@ -249,11 +244,11 @@ function resetGame() {
 
 function flashCards() {
     for (i = 0; i < cards.length; i++) {
-        cards[i].children[0].classList.add("showCard")
+        cards[i].children[0].classList.add("showCard");
     }
     setTimeout(function () {
         for (i = 0; i < cards.length; i++) {
-            cards[i].children[0].classList.remove("showCard")
+            cards[i].children[0].classList.remove("showCard");
         }
-    }, 1500)
+    }, 1500);
 }
